0c6776aa35ce3e6394e3bbce2a82bd7f9618d055
SawBawMuThaw <523k0077@student.tdtu.edu.vn>
2025-03-26 13:40:50 +0700

    Add Label and Textbox for Course Name

--- WinFormsStudentApp/WinFormsStudentApp/Form1.Designer.cs
+++ WinFormsStudentApp/WinFormsStudentApp/Form1.Designer.cs
@@ -30,31 +30,54 @@
         {
             this.lblStudentName = new System.Windows.Forms.Label();
             this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCourseName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lblStudentName
             // 
             this.lblStudentName.AutoSize = true;
-            this.lblStudentName.Location = new System.Drawing.Point(139, 101);
+            this.lblStudentName.Location = new System.Drawing.Point(104, 81);
+            this.lblStudentName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblStudentName.Name = "lblStudentName";
-            this.lblStudentName.Size = new System.Drawing.Size(148, 25);
+            this.lblStudentName.Size = new System.Drawing.Size(112, 20);
             this.lblStudentName.TabIndex = 0;
             this.lblStudentName.Text = "Student Name";
             // 
             // txtStudentName
             // 
-            this.txtStudentName.Location = new System.Drawing.Point(311, 95);
+            this.txtStudentName.Location = new System.Drawing.Point(233, 76);
+            this.txtStudentName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtStudentName.Name = "txtStudentName";
-            this.txtStudentName.Size = new System.Drawing.Size(100, 31);
+            this.txtStudentName.Size = new System.Drawing.Size(100, 26);
             this.txtStudentName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Course Name";
+            // 
+            // txtCourseName
+            // 
+            this.txtCourseName.Location = new System.Drawing.Point(233, 128);
+            this.txtCourseName.Name = "txtCourseName";
+            this.txtCourseName.Size = new System.Drawing.Size(100, 26);
+            this.txtCourseName.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.Controls.Add(this.txtCourseName);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtStudentName);
             this.Controls.Add(this.lblStudentName);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -66,6 +89,8 @@
 
         private System.Windows.Forms.Label lblStudentName;
         private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCourseName;
     }
 }
 
