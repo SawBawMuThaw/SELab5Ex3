66d7a4cd13fb3c29caaf197e8b5ef652c3a0457d
MileFisher <broken101284@gmail.com>
2025-03-26 13:35:02 +0700

    Added Label and TextBox for Student Name

--- WinFormsStudentApp/WinFormsStudentApp/Form1.Designer.cs
+++ WinFormsStudentApp/WinFormsStudentApp/Form1.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblStudentName = new System.Windows.Forms.Label();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblStudentName
+            // 
+            this.lblStudentName.AutoSize = true;
+            this.lblStudentName.Location = new System.Drawing.Point(139, 101);
+            this.lblStudentName.Name = "lblStudentName";
+            this.lblStudentName.Size = new System.Drawing.Size(148, 25);
+            this.lblStudentName.TabIndex = 0;
+            this.lblStudentName.Text = "Student Name";
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(311, 95);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(100, 31);
+            this.txtStudentName.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.lblStudentName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblStudentName;
+        private System.Windows.Forms.TextBox txtStudentName;
     }
 }
 
